1178b0baa56fa16b4d060722d1d78a22e6deb767
bendercdf <b3nder@live.ru>
2019-07-25 22:18:07 +0300

    25.07

---
+++ build/precache-manifest.de9154c92818e48b3417e78b31b631a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cc1158ba3a23227107a4f2fb7fb4b8c",
+    "url": "/static/media/waitAnimation.4cc1158b.svg"
+  },
+  {
+    "revision": "59d41ed3ead52dc1b073211bf1ad97e4",
+    "url": "/static/media/topimage.59d41ed3.jpg"
+  },
+  {
+    "revision": "18972afd99cb2de98ff810a3d2adcfd6",
+    "url": "/static/media/logo.18972afd.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a86e57a920101c22e8be",
+    "url": "/static/js/main.a86e57a9.chunk.js"
+  },
+  {
+    "revision": "13b1cc4519d4b68e3a8c",
+    "url": "/static/js/2.13b1cc45.chunk.js"
+  },
+  {
+    "revision": "a86e57a920101c22e8be",
+    "url": "/static/css/main.a59b1b40.chunk.css"
+  },
+  {
+    "revision": "2c265a93004f9af5d2ffc289e31e9def",
+    "url": "/index.html"
+  }
+];
