faa3f28d4c1351b5625aaa59546ad5fd642b1b58
bendercdf <b3nder@live.ru>
2019-07-25 23:30:43 +0300

    Updates

---
+++ precache-manifest.c688e553c024520873d672a7d666301d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cc1158ba3a23227107a4f2fb7fb4b8c",
+    "url": "/firstproject/static/media/waitAnimation.4cc1158b.svg"
+  },
+  {
+    "revision": "59d41ed3ead52dc1b073211bf1ad97e4",
+    "url": "/firstproject/static/media/topimage.59d41ed3.jpg"
+  },
+  {
+    "revision": "18972afd99cb2de98ff810a3d2adcfd6",
+    "url": "/firstproject/static/media/logo.18972afd.png"
+  },
+  {
+    "revision": "fcf0c7743edcff641f64",
+    "url": "/firstproject/static/js/runtime~main.fcf0c774.js"
+  },
+  {
+    "revision": "75044f813e9413155d87",
+    "url": "/firstproject/static/js/main.75044f81.chunk.js"
+  },
+  {
+    "revision": "13b1cc4519d4b68e3a8c",
+    "url": "/firstproject/static/js/2.13b1cc45.chunk.js"
+  },
+  {
+    "revision": "75044f813e9413155d87",
+    "url": "/firstproject/static/css/main.a59b1b40.chunk.css"
+  },
+  {
+    "revision": "8a9a9ebb8ee37c89cd0f7940225e65cc",
+    "url": "/firstproject/index.html"
+  }
+];
